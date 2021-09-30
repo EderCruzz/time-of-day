2bd93f787d760a000263a84e9197c0bd22181192
Eder Cruz <eder.ales10@gmail.com>
2021-09-30 13:30:37 -0300

    conteudo

---
+++ script.js
@@ -0,0 +1,36 @@
+function carregar() {
+
+var msg = document.getElementById('hora')
+var img = document.getElementById('imagem')
+var apresent = document.getElementById('apresent')
+var agora = new Date()
+var horario = agora.getHours()
+
+if (horario < 5){
+    apresent.innerHTML = 'Boa madrugada!'
+}else if (horario < 12 ) {
+    apresent.innerHTML = 'Bom dia!'
+} else if (horario < 18) {
+    apresent.innerHTML = 'Boa tarde!'
+} else if (horario <= 23) {
+    apresent.innerHTML = 'Boa noite!'
+}
+
+msg.innerHTML = `Agora são ${horario} horas...`
+
+if (horario < 5){
+    img.src = 'assets/madrugada-250.jpg'
+    document.body.style.background = '#303030'
+}else if (horario < 12 ) {
+    img.scr = 'assets/manhã-250.jpg'
+    document.body.style.background = 'skyblue'
+} else if (horario < 18) {
+    img.src = 'assets/tarde-250.jpg'
+    document.body.style.background = '#a16244'
+} else if (horario <= 23) {
+    img.src = 'assets/noite-250.jpg'
+    document.body.style.background = '#2c2736'
+} else {
+    alert('[ERRO]')
+}
+}
